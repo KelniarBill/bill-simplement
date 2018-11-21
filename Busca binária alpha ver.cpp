1704711054fd2b533a5f4ad0408a1b05481ac918
KelniarBill <44869615+KelniarBill@users.noreply.github.com>
2018-11-21 15:50:04 -0200

    Update Busca binária alpha ver.cpp

--- Busca binária alpha ver.cpp
+++ Busca binária alpha ver.cpp
@@ -1,24 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int buscaBin (int vet[], int n, int x);
-int mostraVet(int vet[]);
+int bolha(int vetor[], int num, int aux, int k, int j);
+int buscaBin(int vetor[], int num, int x);
+int mostraVet(int vetor[]);
 
-int buscaBin (int vet[], int n, int *x){
-	
-	int i=0; 
-	int fim= n-1;
-	int meio= 0;
-	
-	printf ("\n digite o tamanho do vetor");
-	scanf ("%d", &n);
-	
-	while (i <= fim){
-		if (*x == vet[meio]){
+int bolha(int vet[], int num, int aux, int k, int j) {
+	for (k = 0; k < num; k++) {
+		printf("\n[%d]", k);
+		for (j = 0; j < num - 1; j++) {
+			printf("%d,", j);
+
+			if (vetor[j] > vetor[j + 1]) {
+				aux = vetor[j];
+				vetor[j] = vetor[j + 1];
+				vetor[j + 1] = aux;
+			}
+		}
+	}
+}
+
+int buscaBin(int vet[], int n, int *x) {
+
+	int i = 0;
+	int fim = n - 1;
+	int meio = 0;
+
+	printf("\n digite o tamanho do vetor");
+	scanf("%d", &n);
+
+	while (i <= fim) {
+		if (*x == vet[meio]) {
 			return 1;
-		}else{
-			if (*x < vet[meio]){
+		}
+		else {
+			if (*x < vet[meio]) {
 				fim = (meio - 1);
-			}else{
+			}
+			else {
 				i = (meio + 1);
 			}
 		}
@@ -26,22 +45,22 @@ int buscaBin (int vet[], int n, int *x){
 	return 0;
 }
 
-int mostraVet(int *n, int vet[]){
-	
+int mostraVet(int *n, int vet[]) {
+
 	printf("\n O vetor eh: %d", vet[*n]);
 }
 
-int main (void){
+int main(void) {
 	int *n;
-	int *i; 
+	int *i;
 	int *fim;
 	int *meio;
 	int *x;
-	
+
 	int buscaBin();
 	int mostraVet();
-	
-	
-	
+
+
+
 	return 0;
 }
